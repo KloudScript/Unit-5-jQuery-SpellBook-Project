5fd74be1109d4206ace5fea78aaa33a8f0788b1a
Christian <43751846+KloudScript@users.noreply.github.com>
2019-01-14 10:08:03 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,25 @@
+//Spell #1
+
+$("#spellbutton1").click(function(){
+  $("#spell1").attr("src","https://vignette.wikia.nocookie.net/wizardsofwaverlyplace/images/2/2e/Maximan1.jpg/revision/latest?cb=20090225185550");
+});
+
+
+
+//Spell #2
+
+$("#spellbutton2").click(function(){
+  $("#spell2").attr("src","https://livingandworkinginmexico.files.wordpress.com/2010/02/nevado-con-mucha-nieve-1.jpg");
+});
+
+
+
+
+//Spell #3
+
+$("      ").click(function(){
+ 
+});
+
+
+
