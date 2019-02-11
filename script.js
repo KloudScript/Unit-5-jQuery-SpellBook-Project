c5a7598a7c7e5ee2dcb5c1205d64f334e6537e0f
Christian <43751846+KloudScript@users.noreply.github.com>
2019-02-11 10:03:17 -0500

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -17,9 +17,17 @@ $("#spellbutton2").click(function(){
 
 //Spell #3
 
-$("      ").click(function(){
- 
+$("#spellbutton3").click(function(){
+      $("#spell3").attr("src","https://dayair.org/wp-content/uploads/2017/04/money-wallet.jpg");
 });
 
+$("#logo").click(function(){
+      $("#logo").attr("src","https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/wizards-of-waverly-place-10th-anniversary-cast-pic-1507155805.jpg?crop=1.00xw:0.752xh;0,0&resize=480:*");
+});
+
+$("#spell4").click(function(){
+      $("#spell4").text("Thanks! I hope You Enjoyed My Spell Book!");
+      $("#spell4").css("color", "maroon");
+});
 
 
